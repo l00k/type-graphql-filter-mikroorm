17696ae822660b466bda1118533e55f7c39cbdc8
Lukasz Dobrowolski <l00k+github@protonmail.com>
2021-04-11 03:08:50 +0200

    additional filter option

--- lib/types/filterOperator.ts
+++ lib/types/filterOperator.ts
@@ -20,6 +20,8 @@ export type LogicalOperator =
   | "or"
   | "not"
 
+export const FILTER_OPERATORS: (LogicalOperator|FilterOperator)[] = [ "eq", "gt", "gte", "in", "lt", "lte", "ne", "nin", "like", "re", "ilike", "overlap", "contains", "contained", "and", "or", "not" ];
+
 export const LOGICAL_RETURN_TYPE_OPERATORS: LogicalOperator[] = ["and", "or", "not"];
 
 export const ARRAY_RETURN_TYPE_OPERATORS: (LogicalOperator|FilterOperator)[] = ["and", "or", "in"];
